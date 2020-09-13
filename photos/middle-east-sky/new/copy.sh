4967b317a99742ef042a2587392375565f9f0c1a
Adeel Ahmad <adeelahmad14@hotmail.com>
2020-09-13 22:43:44 +0200

    Update index page and timezone

--- photos/middle-east-sky/new/copy.sh
+++
@@ -1,4 +0,0 @@
-cp /home/adeel/photos/switzerland/airline-dubai-geneva-28-08-2018/pixel-xl/IMG_20200828_152153.jpg .
-cp /home/adeel/photos/switzerland/airline-dubai-geneva-28-08-2018/pixel-xl/IMG_20200828_064512.jpg .
-cp /home/adeel/photos/switzerland/airline-dubai-geneva-28-08-2018/pixel-xl/IMG_20200828_104018.jpg .
-cp /home/adeel/photos/switzerland/airline-dubai-geneva-28-08-2018/pixel-xl/IMG_20200828_115833.jpg .
